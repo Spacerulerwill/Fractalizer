80f8b7fc51fe8b59d3539d57ac8b861d471f7300
tom <83908974+goat6@users.noreply.github.com>
2023-11-26 11:26:26 +0000

    Abstracted opengl init & added types

---
+++ fractalizer/src/lib/interfaces.ts
@@ -0,0 +1,7 @@
+export interface ProgramInfo {
+    gl: WebGLRenderingContext | null;
+    zoomLoc: WebGLUniformLocation | null;
+    resolutionLoc: WebGLUniformLocation | null;
+    locationLoc: WebGLUniformLocation | null;
+    fractalTypeLoc: WebGLUniformLocation | null;
+  }
