c241fd02e84f04c7a09c65db5490f0a6eebed3d1
tom <83908974+goat6@users.noreply.github.com>
2023-11-26 15:14:57 +0000

    save image toast

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
   import { Fractals } from "$lib/interfaces";
-  import {
-    AppBar,
-    LightSwitch,
-    RadioGroup,
-    RadioItem,
-  } from "@skeletonlabs/skeleton";
+  import { RadioGroup } from "@skeletonlabs/skeleton";
 
   export let changeFractal: (fractal: number) => void;
   export let selectedFractal: number;
   export let zoom: number;
   export let fractalX: number;
   export let fractalY: number;
-  export let getScreenshot: () => void;
+  // export let getScreenshot: () => void;
   export let saveImage: () => void;
 
   const length = Object.keys(Fractals).length / 2;
 </script>
 
-<menu class="w-full h-full absolute bottom-0 select-none">
+<menu class="w-screen h-screen absolute bottom-0 select-none flex">
   <nav class="w-full p-8 font-semibold h3">
     <a
       href="https://github.com/Spacerulerwill/Fractalizer"
@@ -31,22 +26,10 @@
     <p class="gradient-heading">X: {fractalX.toFixed(2)}</p>
     <p class="gradient-heading">Y: {fractalY.toFixed(2)}</p>
   </nav>
-  <div
-    class="absolute bottom-0 w-full text-3xl px-80 gap-1 h-20 flex justify-around items-center"
-  >
-    {#each { length: length } as _, index (index)}
-      <button
-        class="{selectedFractal === index
-          ? 'variant-ghost-primary'
-          : 'variant-ghost-secondary'} p-4 w-full"
-        on:click={() => changeFractal(index)}
-      >
-        {Fractals[index]}
-      </button>
-    {/each}
+  <div class="flex gap-2 items-center h-fit">
     <svg
       on:click={saveImage}
-      class="w-20 h-20 text-gray-800 dark:text-white"
+      class="w-10 h-10 text-gray-800 dark:text-white"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -67,5 +50,20 @@
         d="M17 3h-2l-.447-.894A2 2 0 0 0 12.764 1H7.236a2 2 0 0 0-1.789 1.106L5 3H3a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V5a2 2 0 0 0-2-2Z"
       />
     </svg>
+    <RadioGroup
+      hover="true"
+      rounded="rounded-container-token"
+      display="flex-col"
+    >
+      {#each { length: length } as _, index (index)}
+        <option
+          class={selectedFractal === index ? "variant-ghost-primary" : null}
+          value={index}
+          on:click={() => changeFractal(index)}
+        >
+          {Fractals[index]}
+        </option>
+      {/each}
+    </RadioGroup>
   </div>
 </menu>
